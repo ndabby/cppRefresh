fa2d7ba8143e0dafa2fbb03d1d1a9d4a79c664a1
ndabby <nadinedabby@unknown002608df8456.att.net>
2013-12-16 16:24:32 -0800

    Initial Commit

---
+++ test/cppTest/cppTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cppTest
+//
+//  Created by Nadine Dabby on 12/16/13.
+//  Copyright (c) 2013 Nadine Dabby. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
